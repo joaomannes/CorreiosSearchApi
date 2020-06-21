c93d95bd9d5b4a49bc001abba1a2b83c8aa1f3bf
João Gabriel <joaogm@joaogm.com.br>
2020-06-21 19:58:38 -0300

    Commit Inicial

---
+++ CorreiosSearchRest.API/Controllers/CepController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CorreiosSearchRest.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorreiosSearchRest.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CepController : ControllerBase
+    {
+
+        public CepController()
+        {
+
+        }
+
+        [HttpGet("{cep}")]
+        public async Task<IActionResult> Index([FromRoute]string cep)
+        {
+            if (string.IsNullOrWhiteSpace(cep))
+                return BadRequest("Informe o CEP a ser consultado");
+
+            try
+            {
+                var correios = new Correios.AtendeClienteClient();
+
+                var consulta = await correios.consultaCEPAsync(cep.Replace("-", ""));
+
+                var response = ConsultaCepModel.FromCorreiosResponse(consulta);
+
+                return Ok(response);
+            } catch
+            {
+                return BadRequest("Erro ao consultar o cep informado");
+            }
+            
+        }
+    }
+}
---
+++ CorreiosSearchRest.API/Models/ConsultaCepModel.cs
@@ -0,0 +1,31 @@
+﻿using Correios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CorreiosSearchRest.API.Models
+{
+    public class ConsultaCepModel
+    {
+        public string Cep { get; set; }
+        public string Endereco { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Uf { get; set; }
+
+        public static ConsultaCepModel FromCorreiosResponse(consultaCEPResponse response)
+        {
+            return new ConsultaCepModel
+            {
+                Bairro = response.@return.bairro,
+                Cep = response.@return.cep,
+                Cidade = response.@return.cidade,
+                Complemento = response.@return.complemento2,
+                Endereco = response.@return.end,
+                Uf = response.@return.uf
+            };
+        }
+    }
+}
